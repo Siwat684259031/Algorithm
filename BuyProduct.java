9fb3c20ad8ee583dd73ddcccb4851d5ccef64555
Siwat684259031 <684259031@webmail.npru.ac.th>
2025-09-02 15:14:59 +0700

    Siwat

--- BuyProduct.java
+++ BuyProduct.java
@@ -4,31 +4,42 @@ public class BuyProduct{
 
          Scanner scanner =new Scanner(System.in);
 
-
-         System.out.print("Enter productName1 : ");
+         System.out.print("productName1 : ");
          String productName1 = scanner.nextLine();
-
-         System.out.print("Enter productName2 : ");
-         String productName2 = scanner.nextLine();
-
-         System.out.print("Enter productName3 : ");
-         String productName3 = scanner.nextLine();
-
-         System.out.print("Enter product1 : ");
+         System.out.printf("product1 : ");
          double product1 = scanner.nextDouble();
+         scanner.nextLine();
 
-         System.out.print("Enter product2 : ");
+         System.out.print("productName2 : ");
+         String productName2 = scanner.nextLine();
+         System.out.printf("product2 : ");
          double product2 = scanner.nextDouble();
+         scanner.nextLine();
 
-         System.out.print("Enter product3 : ");
+         System.out.print("productName3 : ");
+         String productName3 = scanner.nextLine();
+         System.out.printf("product3 : ");
          double product3 = scanner.nextDouble();
+         scanner.nextLine();
 
-         double totalPricre = product1+product2 ;
+         System.out.print("Customer paid money : ");
+         double money = scanner.nextDouble();
+
+         System.out.println("-----------Receipt-----------");
+         double totalPricre = product1+product2+product3 ;
          double VAT = totalPricre*0.07 ;
+         double totalWithTax =  totalPricre +VAT ;
+         double change = money - totalWithTax ;
 
-         
-         
 
+         System.out.println(productName1+":" +product1+ " bath");
+         System.out.println(productName2+":" +product2+ " bath");
+         System.out.println(productName3+":" +product3+ " bath");
+         System.out.println("Total pricre before tax "+totalPricre+ " bath");
+         System.out.println("VAT = "+VAT+ " bath");
+         System.out.println("totalWithTax "+totalWithTax+ " bath");
+         System.out.println("totol paid "+money+ " bath");
+         System.out.println("Change "+change+ " bath");
 
     }
 }
